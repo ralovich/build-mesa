27bc5c37b657e812a834c766206ec76ae67accb4
Kristof Ralovich <tade60@freemail.hu>
2026-09-21 22:23:18 +0200

    mesa 25.1.0

--- build.sh
+++ build.sh
@@ -15,7 +15,7 @@ LLVM_TARGETS_TO_BUILD=AArch64
 TARGET_ARCH_NAME=aarch64
 
 wget -c -nv https://archive.mesa3d.org/mesa-${MESA_VERSION}.tar.xz
-echo "96a53501fd59679654273258c6c6a1055a20e352ee1429f0b123516c7190e5b0 mesa-${MESA_VERSION}.tar.xz" | sha256sum -c
+echo "b1c45888969ee5df997e2542654f735ab1b772924b442f3016d2293414c99c14 mesa-${MESA_VERSION}.tar.xz" | sha256sum -c
 tar -xJf mesa-${MESA_VERSION}.tar.xz
 
 wget -c -nv https://github.com/llvm/llvm-project/releases/download/llvmorg-${LLVM_VERSION}/llvm-${LLVM_VERSION}.src.tar.xz

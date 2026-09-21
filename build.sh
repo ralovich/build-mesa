f70a23124584d607022305c21c1e61a40d2490e7
Kristof Ralovich <tade60@freemail.hu>
2026-09-22 01:15:16 +0200

    llvm 22.1.8

--- build.sh
+++ build.sh
@@ -6,7 +6,7 @@ set -x # echo commands
 
 # brew install glslang
 
-LLVM_VERSION=21.1.8
+LLVM_VERSION=22.1.8
 MESA_VERSION=25.2.0
 
 MESA_ARCH=arm64
@@ -19,7 +19,7 @@ echo "7c726b21c074d14d31d253d638b741422f3c0a497ce7f1b4aaaa917d10bd8d4f mesa-${ME
 tar -xJf mesa-${MESA_VERSION}.tar.xz
 
 wget -c -nv https://github.com/llvm/llvm-project/releases/download/llvmorg-${LLVM_VERSION}/llvm-project-${LLVM_VERSION}.src.tar.xz
-echo "4633a23617fa31a3ea51242586ea7fb1da7140e426bd62fc164261fe036aa142 llvm-project-${LLVM_VERSION}.src.tar.xz" | sha256sum -c
+echo "922f1817a0df7b1489272d18134ee0087a8b068828f87ac63b9861b1a9965888 llvm-project-${LLVM_VERSION}.src.tar.xz" | sha256sum -c
 tar -xJf llvm-project-${LLVM_VERSION}.src.tar.xz
 
 (
